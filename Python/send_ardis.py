d691e0addce44a54d8c5750f69a6ce5b634f8abb
Gabriel Niziolek <ganiziolek@gmail.com>
2023-09-27 15:50:08 -0300

    envia figure do layout

--- Python/send_ardis.py
+++ Python/send_ardis.py
@@ -1,13 +1,17 @@
-from requests import Session
-from requests.exceptions import HTTPError
 from argparse import ArgumentParser
+import pathlib
 import logging
 from logging.handlers import RotatingFileHandler
-from pandas import read_csv
 from urllib.parse import urljoin
-import PySimpleGUI as sg
 import tempfile
 import sys
+import os
+import base64
+
+from requests import Session
+from requests.exceptions import HTTPError
+from pandas import read_csv
+import PySimpleGUI as sg
 
 sg.theme('Dark Blue 3')
 
@@ -82,6 +86,12 @@ parser.add_argument(
     help='Impede o programa de continuar se o id único de uma peça já está inserido em algum plano ativo',
 )
 
+parser.add_argument(
+    '--figures-directory',
+    type=pathlib.Path,
+    help='Diretório onde as figures de cada plano serão buscadas. O nome de cada arquivo deve ser igual ao código do plano, com extensão .png',
+)
+
 args = parser.parse_args()
 
 logger.debug('Argumentos: %s', args)
@@ -133,19 +143,9 @@ def get_auth_key():
         raise SystemExit from exc
 
 def envia_layouts(layouts):
-    # Envia os layouts
-    for row_num, layout in enumerate(layouts.itertuples(index=True)):
-        logger.debug('Enviando layout: %s',layout)
-
-        if not sg.one_line_progress_meter(
-                                    'Enviando Layouts',
-                                    row_num, len(layouts.index),
-                                    f'Codigo layout: {layout.codigo_layout}',
-                                    orientation='h',
-                                    no_titlebar=False,
-                                    grab_anywhere=False):
-            break
     
+    def envia_layout(layout):
+
         plano_de_corte = {
             "codigo_layout":       layout.codigo_layout,
             "qtd_chapas":          layout.qtd_chapas,
@@ -161,7 +161,7 @@ def envia_layouts(layouts):
             "sobra":               layout.sobra == 'S'
         }
 
-        logger.info(f'Cadastrando plano_de_corte {layout.codigo_layout}')
+        logger.info('Cadastrando plano_de_corte %s', layout.codigo_layout)
 
         res = s.post(
             url=URL_PLANO_DE_CORTE,
@@ -170,30 +170,90 @@ def envia_layouts(layouts):
 
         try:
             res.raise_for_status()
-        except HTTPError as exc:
+        except HTTPError as err:
 
             logger.exception('')
 
             try:
-                mensagem = exc.response.json().get('mensagem')
+                mensagem = err.response.json().get('mensagem')
             except Exception:
                 mensagem = 'A API não enviou uma mensagem de erro, verifique os logs.'
 
             logger.error(mensagem)
 
-            response = sg.Popup(
-                            f'Codigo layout: {layout.codigo_layout}',
-                            'Ocorreu um erro ao enviar esse layout:',
-                            mensagem,
-                            f'Log completo em: {log_file}',
-                            title='Erro ao enviar Layouts',
-                            button_type=sg.POPUP_BUTTONS_OK
-                        )
+            sg.Popup(
+                f'Codigo layout: {layout.codigo_layout}',
+                'Ocorreu um erro ao enviar esse layout:',
+                mensagem,
+                f'Log completo em: {log_file}',
+                title='Erro ao enviar Layouts',
+                button_type=sg.POPUP_BUTTONS_OK
+            )
             
             sg.one_line_progress_meter_cancel()
-            raise SystemExit from exc
-        else:
-            logger.debug('ok')
+            raise SystemExit from err
+
+    def envia_layout_figure(layout):
+
+        logger.info('Enviando figure do plano_de_corte %s', layout.codigo_layout)
+
+        figure_path = os.path.join(args.figures_directory, f'{layout.codigo_layout}.png')
+
+        if not os.path.exists(figure_path):
+            logger.warning('O arquivo "%s" não foi encontrado', figure_path)
+            return
+        
+        with open(figure_path, 'rb') as figure_img:
+
+            encoded_str = base64.b64encode(figure_img.read())
+
+            res = s.post(
+                url=urljoin(args.host, f'plano-de-corte/{layout.codigo_layout}/figure'),
+                json={
+                    'decoded_figure': encoded_str.decode('utf-8')
+                }
+            )
+
+            try:
+                res.raise_for_status()
+            except HTTPError as err:
+
+                logger.exception('')
+
+                try:
+                    mensagem = err.response.json().get('mensagem')
+                except Exception:
+                    mensagem = 'A API não enviou uma mensagem de erro, verifique os logs.'
+
+                logger.error(mensagem)
+
+                sg.Popup(
+                    f'Codigo layout: {layout.codigo_layout}',
+                    'Ocorreu um erro ao enviar a figure desse layout:',
+                    mensagem,
+                    f'Log completo em: {log_file}',
+                    title='Erro ao enviar Layouts',
+                    button_type=sg.POPUP_BUTTONS_OK
+                )
+            else:
+                logger.info('ok')
+
+    for row_num, layout in enumerate(layouts.itertuples(index=True)):
+        logger.debug('Enviando layout: %s',layout)
+
+        if not sg.one_line_progress_meter(
+                                    'Enviando Layouts',
+                                    row_num, len(layouts.index),
+                                    f'Codigo layout: {layout.codigo_layout}',
+                                    orientation='h',
+                                    no_titlebar=False,
+                                    grab_anywhere=False):
+            break
+    
+        envia_layout(layout)
+
+        if args.figures_directory:
+            envia_layout_figure(layout)
 
     sg.one_line_progress_meter_cancel()
 
@@ -217,7 +277,7 @@ def envia_pecas(parts):
             "tempo_corte_segundos":  str(part.tempo_corte_segundos)
         }
 
-        logger.info(f'Cadastrando Peca {part.id_unico_peca}')
+        logger.info('Cadastrando Peca %s', part.id_unico_peca)
 
         res = s.post(
             url=URL_PLANO_DE_CORTE_PECAS.format(codigo_layout = part.codigo_layout),
@@ -226,12 +286,12 @@ def envia_pecas(parts):
 
         try:
             res.raise_for_status()
-        except HTTPError as exc:
+        except HTTPError as err:
 
             logger.exception('')
 
             try:
-                mensagem = exc.response.json().get('mensagem')
+                mensagem = err.response.json().get('mensagem')
             except Exception:
                 mensagem = 'A API não enviou uma mensagem de erro, verifique os logs.'
 
@@ -250,7 +310,7 @@ def envia_pecas(parts):
                         )
             if response == 'No' or not response:
                 sg.one_line_progress_meter_cancel()
-                raise SystemExit from exc
+                raise SystemExit from err
         else:
             logger.debug('ok')
 
@@ -279,12 +339,12 @@ def verifica_duplicidade_pecas(parts):
 
         try:
             res.raise_for_status()
-        except HTTPError as exc:
+        except HTTPError as err:
 
             logger.exception('')
 
             try:
-                mensagem = e.response.json().get('mensagem')
+                mensagem = err.response.json().get('mensagem')
             except Exception:
                 mensagem = 'A API não enviou uma mensagem de erro, verifique os logs.'
 
@@ -303,7 +363,7 @@ def verifica_duplicidade_pecas(parts):
                         )
             if response == 'No' or not response:
                 sg.one_line_progress_meter_cancel()
-                raise SystemExit from exc
+                raise SystemExit from err
             
             continue
             
@@ -318,12 +378,12 @@ def verifica_duplicidade_pecas(parts):
                 # O Plano que ja foi enviado NÃO é um plano de LOTE
                 continue
 
-            if plano.get('PlanoDeCorte').get('inativo') == True:
+            if plano.get('PlanoDeCorte').get('inativo') is True:
                 # Plano NÃO está Ativo
                 continue
 
             if (
-                    plano.get('PlanoDeCorte').get('finalizado') == False # Plano NÃO foi cortado
+                    plano.get('PlanoDeCorte').get('finalizado') is False # Plano NÃO foi cortado
                         or 
                     not str(part.codigo_layout).startswith(('RETR', 'ASS')) # O Plano atual é um plano de LOTE 
                 ):
@@ -332,7 +392,7 @@ def verifica_duplicidade_pecas(parts):
                             f'ID Único: {part.id_unico_peca} \n'
                             'Essa peça já está inserida no seguinte plano de corte:',
                             f'{plano.get("PlanoDeCorte").get("codigo_layout")} - {plano.get("PlanoDeCorte").get("nome_projeto")}\n',
-                            "Finalizado: Sim\n" if plano.get("PlanoDeCorte").get("finalizado") == True else "Finalizado: Não\n",
+                            "Finalizado: Sim\n" if plano.get("PlanoDeCorte").get("finalizado") is True else "Finalizado: Não\n",
                             'Processo será interrompido!',
                             title='Peça já foi inserida',
                             button_type=sg.POPUP_BUTTONS_OK

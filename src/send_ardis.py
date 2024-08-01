1f233b78c353178b3310fec4877bdb6fe74cd537
Gabriel Niziolek <ganiziolek@gmail.com>
2024-08-01 14:36:00 +0000

    v2.0.2 - Corrigindo problema com modulo utils

--- src/send_ardis.py
+++ src/send_ardis.py
@@ -10,15 +10,16 @@ import sys
 from typing import List
 from httpx import HTTPStatusError, Timeout
 from pydantic import ValidationError
+from typing import Optional
+from pydantic import BaseModel
 
 from pandas import read_csv
 import PySimpleGUI as sg
-from utils import PlanoDeCorteCsvModel, PlanoDeCortePecasCsvModel
-from tx.modules.plano_de_corte.types import PlanoDeCorteCreateModel, PlanoDeCortePecasCreateModel
+from tx.modules.plano_de_corte.types import PlanoDeCorteCreateModel, PlanoDeCortePecasCreateModel, TipoMateriaPrima
 
 from tx.tx import Tx
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 sg.theme("Dark Blue 3")
 
@@ -112,6 +113,30 @@ parser.add_argument(
 args = parser.parse_args()
 
 logger.debug("Argumentos: %s", args)
+
+
+class PlanoDeCortePecasCsvModel(BaseModel):
+    codigo_layout: str
+    qtd_cortada_no_layout: int
+    id_unico_peca: Optional[int]
+    id_ordem: Optional[int]
+    tempo_corte_segundos: float
+
+
+class PlanoDeCorteCsvModel(BaseModel):
+    codigo_layout: str
+    descricao_material: str
+    id_recurso: int
+    mm_comp_linear: float
+    mm_comprimento: float
+    mm_largura: float
+    nome_projeto: str
+    perc_aproveitamento: float
+    perc_sobras: float
+    tipo: TipoMateriaPrima
+    codigo_lote: str
+    qtd_chapas: int
+    tempo_estimado_seg: float
 
 
 def show_error_popup(msg: str, exc: Exception):

f1add1a4854e37103d70590cc6a3bf5f1ee8abda
LuisFelipeRodrigo <luisfelipe2014355@gmail.com>
2025-05-07 14:56:42 +0000

    validacao-erros-ao-apontar-leitura-furadeiras

--- src/subcommands/apontar_leitura_furadeira_nanxing.py
+++ src/subcommands/apontar_leitura_furadeira_nanxing.py
@@ -10,27 +10,44 @@ from src.tx.tx import Tx
 
 logger = logging.getLogger("src.subcommands.apontar_leitura_furadeira_nanxing")
 
-def obter_ultimo_csv(diretorio: Path) -> Path:
+def obter_ultimo_csv(diretorio: Path):
     arquivos_csv = [
         p for p in diretorio.glob("*.csv")
-        if "_PROCESSADO_TEMPOX" not in p.stem
+        if "_PROCESSADO_TEMPOX" not in p.stem and "_COM_ERRO_TEMPOX" not in p.stem
     ]
     if not arquivos_csv:
         raise FileNotFoundError("Nenhum arquivo CSV encontrado.")
 
     return max(arquivos_csv, key=lambda p: p.stat().st_mtime)
 
-def carregar_linhas_processadas(arquivo_processado: Path) -> set:
+def obter_ultimo_csv_com_erro(diretorio: Path):
+    arquivos_csv = [
+        p for p in diretorio.glob("*.csv")
+        if "_COM_ERRO_TEMPOX" not in p.stem
+    ]
+    if not arquivos_csv:
+        raise FileNotFoundError("Nenhum arquivo com ERRO encontrado.")
+
+    return max(arquivos_csv, key=lambda p: p.stat().st_mtime)
+
+def carregar_linhas_processadas(arquivo_processado: Path):
     if not arquivo_processado.exists():
         return set()
 
     with arquivo_processado.open("r", newline="", encoding="utf-8") as f:
         reader = csv.reader(f)
         return set(tuple(row) for row in reader)
+    
+def carregar_linhas_com_erro(arquivo_com_erro: Path):
+    if not arquivo_com_erro.exists():
+        return set()
 
+    with arquivo_com_erro.open("r", newline="", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        return set(tuple(row) for row in reader)
 
 def apontar_leitura_furadeira_nanxing_subcommand(parsed_args: Namespace):
-    logger.info("Iniciando leitura dos planos no MES...")
+    logger.info("Iniciando processo de apontamento de leituras no MES...")
     tx = Tx(
         base_url=parsed_args.host,
         user=parsed_args.user,
@@ -40,56 +57,71 @@ def apontar_leitura_furadeira_nanxing_subcommand(parsed_args: Namespace):
 
     diretorio = Path(parsed_args.caminho_arquivo)
     while True:
+        logger.info("Aguardando próximo ciclo (1 segundos)...")
+        time.sleep(1)
         try:
             csv_entrada = obter_ultimo_csv(diretorio)
+            csv_com_erro = obter_ultimo_csv(diretorio)
+           
             nome_processado = csv_entrada.stem + "_PROCESSADO_TEMPOX.csv"
             caminho_processado = csv_entrada.with_name(nome_processado)
-
             linhas_processadas = carregar_linhas_processadas(caminho_processado)
+
+            nome_com_erro = csv_com_erro.stem + "_COM_ERRO_TEMPOX.csv"
+            caminho_com_erro = csv_com_erro.with_name(nome_com_erro)
+            linhas_com_erro = carregar_linhas_com_erro(caminho_com_erro)
+
 
             with csv_entrada.open("r", newline="", encoding="utf-8") as f_in:
                 reader = csv.reader(f_in)
                 linhas = list(reader)[1:]  # Ignora cabeçalho
 
-            with caminho_processado.open("a", newline="", encoding="utf-8") as f_out:
-                writer = csv.writer(f_out)
+            for linha in linhas:
+                if tuple(linha) in linhas_processadas:
+                    logger.debug("Linha já processada, ignorando.")
+                    continue
+                
+                if tuple(linha) in linhas_com_erro:
+                    logger.debug("Linha já processada, ignorando.")
+                    continue
 
-                for linha in linhas:
-                    if tuple(linha) in linhas_processadas:
-                        logger.debug("Linha já processada, ignorando.")
+                try:
+                    path = Path(linha[1])
+                    ord = path.stem
+
+                    if not ord:
+                        logger.warning(
+                            f"ORD inválida na linha: {linha}"
+                        )
                         continue
 
-                    try:
-                        path = Path(linha[1])
-                        ord = path.stem
+                    logger.info(f"Enviando leitura para API: {ord}")
 
-                        if not ord:
-                            logger.warning(
-                                f"ORD inválida na linha: {linha}"
-                            )
-                            continue
+                    leitura = LeiturasPost(
+                        id_recurso=parsed_args.id_recurso,
+                        codigo=ord,
+                        qtd=1,
+                        leitura_manual=False,
+                    )
 
-                        logger.info(f"Enviando leitura para API: {ord}")
+                    tx.leitura.nova_leitura(
+                        id_recurso=leitura.id_recurso,
+                        codigo=leitura.codigo,
+                        qtd=leitura.qtd,
+                        leitura_manual=leitura.leitura_manual,
+                    )
 
-                        leitura = LeiturasPost(
-                            id_recurso=parsed_args.id_recurso,
-                            codigo=ord,
-                            qtd=1,
-                            leitura_manual=False,
-                        )
-
-                        tx.leitura.nova_leitura(
-                            id_recurso=leitura.id_recurso,
-                            codigo=leitura.codigo,
-                            qtd=leitura.qtd,
-                            leitura_manual=leitura.leitura_manual,
-                        )
-
-                        writer.writerow(linha)
+                    with caminho_processado.open("a", newline="", encoding="utf-8") as f_out:
+                        writer_arquivo_processado = csv.writer(f_out)
+                        writer_arquivo_processado.writerow(linha)
                         logger.info(f"Linha processada com sucesso: {linha}")
 
-                    except Exception as e:
-                        logger.error(f"Erro ao processar linha {linha}: {e}")
+                except Exception as e:
+                    logger.error(f"Erro ao processar linha {linha}: {e}")
+                    with caminho_com_erro.open("a", newline="", encoding="utf-8") as f_out:
+                        writer_arquivo_com_erro = csv.writer(f_out)
+                        writer_arquivo_com_erro.writerow(linha)
+                        logger.info(f"Adicionando linha com erro no arquivo de erros: {linha}")
 
         except Exception as erro:
             logger.error(f"Erro no processamento: {erro}")
--- src/subcommands/apontar_leitura_furadeira_scm_pratika.py
+++ src/subcommands/apontar_leitura_furadeira_scm_pratika.py
@@ -1,60 +1,53 @@
 import csv
 import logging
-import os
-import re
 import time
 from argparse import Namespace
 from pathlib import Path
-from typing import Optional
+
 
 from src.tx.modules.leituras.types import LeiturasPost
 from src.tx.tx import Tx
 
 logger = logging.getLogger("src.subcommands.apontar_leitura_furadeira_scm_pratika")
 
-
-def obter_ultimo_pro(diretorio: Path) -> Path:
-    arquivos_pro = [
+def obter_ultimo_csv(diretorio: Path):
+    arquivos_csv = [
         p for p in diretorio.glob("*.pro")
-        if "_PROCESSADO_SCM_PRATIKA" not in p.stem
+        if "_PROCESSADO_TEMPOX" not in p.stem and "_COM_ERRO_TEMPOX" not in p.stem
     ]
-    if not arquivos_pro:
-        raise FileNotFoundError("Nenhum arquivo .pro encontrado.")
+    if not arquivos_csv:
+        raise FileNotFoundError("Nenhum arquivo PRO encontrado.")
 
-    return max(arquivos_pro, key=lambda p: p.stat().st_mtime)
+    return max(arquivos_csv, key=lambda p: p.stat().st_mtime)
 
+def obter_ultimo_csv_com_erro(diretorio: Path):
+    arquivos_csv = [
+        p for p in diretorio.glob("*.pro")
+        if "_COM_ERRO_TEMPOX" not in p.stem
+    ]
+    if not arquivos_csv:
+        raise FileNotFoundError("Nenhum arquivo com ERRO encontrado.")
 
-def extrair_codigos_ordem(arquivo_pro: Path) -> list:
-    padrao_ordem = re.compile(r"ORD\d+#\d+")
-    codigos_ordem = []
-    
-    with arquivo_pro.open("r", encoding="utf-8") as f:
-        for linha in f:
-            if ".PGM" in linha.upper():
-                match = padrao_ordem.search(linha)
-                if match:
-                    codigos_ordem.append(match.group(0))  # Adiciona o código encontrado à lista
-                    
-    return codigos_ordem
+    return max(arquivos_csv, key=lambda p: p.stat().st_mtime)
 
-
-def carregar_codigos_processados_scm_pratika(arquivo_processado: Path) -> set:
+def carregar_linhas_processadas(arquivo_processado: Path):
     if not arquivo_processado.exists():
         return set()
 
     with arquivo_processado.open("r", newline="", encoding="utf-8") as f:
         reader = csv.reader(f)
-        codigos_processados = set()
-        for row in reader:
-            # Supondo que a primeira coluna seja o caminho do arquivo
-            # e as demais sejam os códigos de ordem
-            codigos_processados.update(row[1:])  # Adiciona os códigos de ordem (ignora a primeira coluna)
-        return codigos_processados
+        return set(tuple(row) for row in reader)
+    
+def carregar_linhas_com_erro(arquivo_com_erro: Path):
+    if not arquivo_com_erro.exists():
+        return set()
 
+    with arquivo_com_erro.open("r", newline="", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        return set(tuple(row) for row in reader)
 
 def apontar_leitura_furadeira_scm_pratika_subcommand(parsed_args: Namespace):
-    logger.info("Iniciando leitura dos planos da furadeira SCM Pratika...")
-
+    logger.info("Iniciando processo de apontamento de leituras no MES...")
     tx = Tx(
         base_url=parsed_args.host,
         user=parsed_args.user,
@@ -63,54 +56,75 @@ def apontar_leitura_furadeira_scm_pratika_subcommand(parsed_args: Namespace):
     )
 
     diretorio = Path(parsed_args.caminho_arquivo)
-
     while True:
-        time.sleep(5)
+        logger.info("Aguardando próximo ciclo (1 segundos)...")
+        time.sleep(1)
         try:
-            arquivo_pro = obter_ultimo_pro(diretorio)
-            nome_processado = arquivo_pro.stem + "_PROCESSADO_SCM_PRATIKA.pro"
-            caminho_processado = arquivo_pro.with_name(nome_processado)
+            csv_entrada = obter_ultimo_csv(diretorio)
+            csv_com_erro = obter_ultimo_csv(diretorio)
+           
+            nome_processado = csv_entrada.stem + "_PROCESSADO_TEMPOX.pro"
+            caminho_processado = csv_entrada.with_name(nome_processado)
+            linhas_processadas = carregar_linhas_processadas(caminho_processado)
 
-            codigos_processados = carregar_codigos_processados_scm_pratika(caminho_processado)
+            nome_com_erro = csv_com_erro.stem + "_COM_ERRO_TEMPOX.pro"
+            caminho_com_erro = csv_com_erro.with_name(nome_com_erro)
+            linhas_com_erro = carregar_linhas_com_erro(caminho_com_erro)
 
-            codigos_ordem = extrair_codigos_ordem(arquivo_pro)
-            if not codigos_ordem:
-                logger.warning(f"Não foi possível extrair códigos de ordem do arquivo: {arquivo_pro.name}")
-                continue
 
-            # Verifica se todos os códigos de ordem já foram processados
-            codigos_nao_processados = [codigo for codigo in codigos_ordem if codigo not in codigos_processados]
-            if not codigos_nao_processados:
-                logger.debug(f"Todos os códigos de ordem já processados para o arquivo: {arquivo_pro.name}")
-                time.sleep(5)
-                continue
+            with csv_entrada.open("r", newline="", encoding="utf-8") as f_in:
+                reader = csv.reader(f_in)
+                linhas = list(reader)[1:]  # Ignora cabeçalho
 
-            for codigo_ordem in codigos_nao_processados:
-                logger.info(f"Enviando leitura para API: {codigo_ordem}")
+            for linha in linhas:
+                if tuple(linha) in linhas_processadas:
+                    logger.debug("Linha já processada, ignorando.")
+                    continue
+                
+                if tuple(linha) in linhas_com_erro:
+                    logger.debug("Linha já processada, ignorando.")
+                    continue
 
-                leitura = LeiturasPost(
-                    id_recurso=parsed_args.id_recurso,
-                    codigo=codigo_ordem,
-                    qtd=1,
-                    leitura_manual=False,
-                )
+                try:
+                    tratando_ord = linha[1].strip().strip('"')
+                    ord = Path(tratando_ord.split("\\")[-1]).stem
 
-                tx.leitura.nova_leitura(
-                    id_recurso=leitura.id_recurso,
-                    codigo=leitura.codigo,
-                    qtd=leitura.qtd,
-                    leitura_manual=leitura.leitura_manual,
-                )
+                    if not ord:
+                        logger.warning(
+                            f"ORD inválida na linha: {linha}"
+                        )
+                        continue
 
-            # Escreve no arquivo processado com todos os códigos de ordem, incluindo os novos
-            with caminho_processado.open("a", newline="", encoding="utf-8") as f_out:
-                writer = csv.writer(f_out)
-                writer.writerow([str(arquivo_pro.resolve())] + codigos_ordem)
+                    logger.info(f"Enviando leitura para API: {ord}")
 
-            logger.info(f"Arquivo processado com sucesso: {arquivo_pro.name}")
+                    leitura = LeiturasPost(
+                        id_recurso=parsed_args.id_recurso,
+                        codigo=ord,
+                        qtd=1,
+                        leitura_manual=False,
+                    )
 
-        except Exception as e:
-            logger.error(f"Erro ao processar arquivo: {e}")
+                    tx.leitura.nova_leitura(
+                        id_recurso=leitura.id_recurso,
+                        codigo=leitura.codigo,
+                        qtd=leitura.qtd,
+                        leitura_manual=leitura.leitura_manual,
+                    )
+
+                    with caminho_processado.open("a", newline="", encoding="utf-8") as f_out:
+                        writer_arquivo_processado = csv.writer(f_out)
+                        writer_arquivo_processado.writerow(linha)
+                        logger.info(f"Linha processada com sucesso: {linha}")
+
+                except Exception as e:
+                    logger.error(f"Erro ao processar linha {linha}: {e}")
+                    with caminho_com_erro.open("a", newline="", encoding="utf-8") as f_out:
+                        writer_arquivo_com_erro = csv.writer(f_out)
+                        writer_arquivo_com_erro.writerow(linha)
+                        logger.info(f"Adicionando linha com erro no arquivo de erros: {linha}")
+
+        except Exception as erro:
+            logger.error(f"Erro no processamento: {erro}")
 
         logger.info("Aguardando próximo ciclo (5 segundos)...")
         time.sleep(5)
